32f69269b346d301432e50fa138331093b6975f2
Sohini Joarder <74846050+SohinijRover@users.noreply.github.com>
2020-11-22 09:38:05 +0530

    Pattern_ify
    Level EASY
    Print the following pattern for the given number of rows.
    Pattern for N = 4
    ****
    ***
    **
    *

---
+++ Patterns/Pattern_ify.cpp
@@ -0,0 +1,19 @@
+/*
+Pattern for N = 4
+****
+***
+**
+*
+*/
+
+void printPatt(int n)
+{
+    for(int i=0;i<=n;i++)
+    {
+        for(int j=0;j<n-i;j++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
