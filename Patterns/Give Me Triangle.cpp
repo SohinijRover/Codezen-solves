7808be8ae1481a9457bf1cef90fed9e93ee64b97
Sohini Joarder <74846050+SohinijRover@users.noreply.github.com>
2020-11-22 09:45:37 +0530

    Create Give Me Triangle.cpp

---
+++ Patterns/Give Me Triangle.cpp
@@ -0,0 +1,23 @@
+/*
+Pattern for N = 4
+****
+ ***     
+  **
+   *
+*/
+
+void printPatt(int n)
+{
+    for(int i=0;i<=n;i++)
+    {
+        for(int j=0;j<i;j++)
+        {
+            cout<<" ";
+        }
+        for(int j=0;j<n-i;j++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
