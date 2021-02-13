83f4ab09e6ef770efbf03f784f734886a6de9aa9
Sohini Joarder <74846050+SohinijRover@users.noreply.github.com>
2021-02-14 04:03:34 +0530

    Star Pattern
    Print the following pattern
    Pattern for N = 4
        *
       *** 
      *****
     *******

---
+++ Fundamentals/Star Pattern.cpp
@@ -0,0 +1,34 @@
+/*
+Print the following pattern
+Pattern for N = 4
+    *
+   *** 
+  *****
+ *******
+ */
+ 
+ #include <iostream>
+using namespace std;
+
+int main()
+{
+    int j,n;
+
+    cin>>n;
+
+    for(int i = 1,k = 0;i<=n;++i,k = 0)
+    {
+        for(j= 1;j<=n-i; ++j)
+        {
+            cout <<" ";
+        }
+
+        while(k != 2*i-1)
+        {
+            cout <<"*";
+            ++k;
+        }
+        cout << endl;
+    }    
+    return 0;
+}
