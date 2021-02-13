5ef8683f5c885f105ede38019f3c0c13d65b0c1c
Sohini Joarder <74846050+SohinijRover@users.noreply.github.com>
2021-02-14 04:27:42 +0530

    Incomplete yet
    You are given a string s. Your task is to determine if the given string s contains two non-overlapping substrings "AB" and "BA" (the substrings can go in any order). Print "yes" or "no".
    The string s contains uppercase Latin letters only.

---
+++ STRINGS/Two substrings.cpp
@@ -0,0 +1,5 @@
+/*
+You are given a string s. Your task is to determine, if the given string s contains two non-overlapping substrings "AB" and "BA" (the substrings can go in any order). Print "yes" or "no".
+The string s contains uppercase Latin letters only.
+*/
+
